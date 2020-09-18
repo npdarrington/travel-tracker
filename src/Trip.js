6655d871f3a6ceba019f3e202ad9e9acdaa1055d
Nathan Darrington <npdarrington@icloud.com>
2020-09-18 14:57:02 -0600

    Expanded constructor to set a default duration of 1 day for a trip

--- src/Trip.js
+++ src/Trip.js
@@ -1,8 +1,9 @@
 export default class Trip {
-  constructor(userID, destinationID, travelers = 1, date) {
+  constructor(userID, destinationID, travelers = 1, date, duration = 1) {
     this.userID = userID;
     this.destinationID = destinationID;
     this.travelers = travelers;
     this.date = date;
+    this.duration = duration;
   }
 }
