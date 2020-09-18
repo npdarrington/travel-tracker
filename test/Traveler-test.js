59bccd4a218f75df4c703007e14bffa6facc3059
Nathan Darrington <npdarrington@icloud.com>
2020-09-17 19:29:53 -0600

    Added describe and beforeEach block for Traveler-test.js

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -2,3 +2,11 @@ const { expect } = require("chai");
 
 import travelerData from './test-data/traveler-test-data';
 import Traveler from '../src/Traveler';
+
+describe('Traveler', () => {
+  let traveler;
+
+  beforeEach(() => {
+    traveler = new Traveler(travelerData[0]);
+  });
+});
