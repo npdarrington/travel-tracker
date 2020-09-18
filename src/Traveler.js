6fea519a7c377feebb1de887e8c35e875eedafd9
Nathan Darrington <npdarrington@icloud.com>
2020-09-17 22:04:31 -0600

    Traveler constructor instantiates a traveler with an empty array for pastTrips taken

--- src/Traveler.js
+++ src/Traveler.js
@@ -3,5 +3,9 @@ export default class Traveler {
     this.id = id;
     this.name = name;
     this.travelerType = travelerType;
+    this.pastTrips = [];
   }
+
+  // trips
+  // 0 = upcoming, 1 = pending, 2 = current, 3 = past
 }
