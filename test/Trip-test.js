ab65b64bf74fc8e8abf647bfa8ad76b5efae16a3
Nathan Darrington <npdarrington@icloud.com>
2020-09-18 13:58:36 -0600

    Added import for Trip class and set an instance of a trip

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -3,10 +3,16 @@ const { expect } = require("chai");
 import travelerData from './test-data/traveler-test-data';
 import tripsData from './test-data/trips-test-data';
 import Traveler from '../src/Traveler';
+import Trip from '../src/Trip';
 
 describe('Trip', () => {
-  let traveler;
+  let traveler, trip;
   beforeEach(() => {
     traveler = new Traveler(travelerData[0]);
+    trip = new Trip();
+  });
+
+  describe('Trip Properties', () => {
+    
   });
 })
