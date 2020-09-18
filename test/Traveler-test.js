5b95544fd58f6b0b89741bdccd37ccbc2d034c6b
Nathan Darrington <npdarrington@icloud.com>
2020-09-17 20:10:07 -0600

    Added test for a Traveler to be an instance of Traveler Class

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -10,6 +10,10 @@ describe('Traveler', () => {
     traveler = new Traveler(travelerData[0]);
   });
 
+  it('Should be an instantiation of a traveler', () => {
+    expect(traveler).to.be.an.instanceof(Traveler);
+  });
+
   it('A traveler should have an id', () => {
     console.log(traveler);
     expect(traveler.id).to.eq(1);
