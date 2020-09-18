02fea474876c720c99c06e97a5df6e54480b5711
Nathan Darrington <npdarrington@icloud.com>
2020-09-17 21:56:09 -0600

    Populate with trip test data for testing

---
+++ test/test-data/trips-test-data.js
@@ -0,0 +1,52 @@
+export const trips = [
+  {
+    id:	117,
+    userID:	1,
+    destinationID:	28,
+    travelers:	3,
+    date:	"2021/01/09",
+    duration:	15,
+    status:	"approved",
+    suggestedActivities:	[]
+  },
+  {
+    id:	132,
+    userID:	1,
+    destinationID:	30,
+    travelers:	5,
+    date:	"2020/09/07",
+    duration:	5,
+    status:	"pending",
+    suggestedActivities:	[]
+  },
+  {
+    id:	147,
+    userID:	1,
+    destinationID:	6,
+    travelers:	4,
+    date:	"2019/09/18",
+    duration:	18,
+    status:	"approved",
+    suggestedActivities:	[]
+  },
+  {
+    id:	156,
+    userID:	1,
+    destinationID:	40,
+    travelers:	1,
+    date:	"2019/08/02",
+    duration:	7,
+    status:	"approved",
+    suggestedActivities:	[]
+  },
+  {
+    id:	157,
+    userID:	33,
+    destinationID:	41,
+    travelers:	1,
+    date:	"2020/01/12",
+    duration:	11,
+    status:	"approved",
+    suggestedActivities:	[]
+  }
+]
