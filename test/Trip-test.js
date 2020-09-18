aa0d238d1a01cd30ceb51df7d24e858f53f3ab84
Nathan Darrington <npdarrington@icloud.com>
2020-09-18 13:56:25 -0600

    Set up beforeEach block with a traveler to use for testing

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -3,3 +3,10 @@ const { expect } = require("chai");
 import travelerData from './test-data/traveler-test-data';
 import tripsData from './test-data/trips-test-data';
 import Traveler from '../src/Traveler';
+
+describe('Trip', () => {
+  let traveler;
+  beforeEach(() => {
+    traveler = new Traveler(travelerData[0]);
+  });
+})
