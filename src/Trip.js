e953b2f2b4d2f8cd3625549ff9de24cd23cc34f7
Nathan Darrington <npdarrington@icloud.com>
2020-09-18 14:54:07 -0600

    Expanded constructor to set a date for the trip

--- src/Trip.js
+++ src/Trip.js
@@ -1,7 +1,8 @@
 export default class Trip {
-  constructor(userID, destinationID, travelers = 1) {
+  constructor(userID, destinationID, travelers = 1, date) {
     this.userID = userID;
     this.destinationID = destinationID;
     this.travelers = travelers;
+    this.date = date;
   }
 }
