3ec05940e2d6c87b685f5b65ce8fea475fcd826e
Nathan Darrington <npdarrington@icloud.com>
2020-09-18 13:54:35 -0600

    Added in require and import statements needed for testing Trip.js

---
+++ test/Trip-test.js
@@ -0,0 +1,5 @@
+const { expect } = require("chai");
+
+import travelerData from './test-data/traveler-test-data';
+import tripsData from './test-data/trips-test-data';
+import Traveler from '../src/Traveler';
