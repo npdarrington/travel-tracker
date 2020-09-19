ff706cf1836024b7d4bef312278d2e5f4e6d5c62
Nathan Darrington <npdarrington@icloud.com>
2020-09-18 20:31:46 -0600

    Removed unnecessary tripsData import

--- test/Trip-test.js
+++ test/Trip-test.js
@@ -1,7 +1,6 @@
 const { expect } = require("chai");
 
 import travelerData from './test-data/traveler-test-data';
-import tripsData from './test-data/trips-test-data';
 import destinationData from './test-data/destination-test-data';
 import Traveler from '../src/Traveler';
 import Trip from '../src/Trip';
