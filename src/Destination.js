e119382d864a499f129301abf60646972fb7a27f
Nathan Darrington <npdarrington@icloud.com>
2020-09-19 09:50:06 -0600

    Expanded constructor for a Destination to have an estimatedFlightCostPerPerson

--- src/Destination.js
+++ src/Destination.js
@@ -1,7 +1,8 @@
 export default class Destination {
-  constructor({ id, destination, estimatedLodgingCostPerDay }) {
+  constructor({ id, destination, estimatedLodgingCostPerDay, estimatedFlightCostPerPerson }) {
     this.id = id;
     this.destination = destination;
     this.estimatedLodgingCostPerDay = estimatedLodgingCostPerDay;
+    this.estimatedFlightCostPerPerson = estimatedFlightCostPerPerson;
   }
 }
