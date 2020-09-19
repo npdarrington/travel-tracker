4cbca60648cdd82a6f769c3f63181814b4786f36
Nathan Darrington <npdarrington@icloud.com>
2020-09-19 09:54:22 -0600

    Expanded constructor for a Destination to have an alt tag associated with showcase image

--- src/Destination.js
+++ src/Destination.js
@@ -1,9 +1,10 @@
 export default class Destination {
-  constructor({ id, destination, estimatedLodgingCostPerDay, estimatedFlightCostPerPerson, image }) {
+  constructor({ id, destination, estimatedLodgingCostPerDay, estimatedFlightCostPerPerson, image, alt }) {
     this.id = id;
     this.destination = destination;
     this.estimatedLodgingCostPerDay = estimatedLodgingCostPerDay;
     this.estimatedFlightCostPerPerson = estimatedFlightCostPerPerson;
     this.image = image;
+    this.alt = alt;
   }
 }
