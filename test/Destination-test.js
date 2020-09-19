bc950343dc70ff007c34ff677b396ef51008c69a
Nathan Darrington <npdarrington@icloud.com>
2020-09-19 09:21:01 -0600

    Set up describe Destination block and beforeEach

--- test/Destination-test.js
+++ test/Destination-test.js
@@ -1,3 +1,11 @@
 const { expect } = require("chai");
 
+import Destination from '../src/Destination';
 import destinationData from './test-data/destination-test-data';
+
+describe("Destination", () => {
+  let destination;
+  beforeEach(() => {
+    destination = new Destination();
+  });
+})
