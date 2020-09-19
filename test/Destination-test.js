7f9ec8b102a6afe77722162cf18b602af6db7e74
Nathan Darrington <npdarrington@icloud.com>
2020-09-19 09:24:58 -0600

    Added test for a Destination to have an id

--- test/Destination-test.js
+++ test/Destination-test.js
@@ -6,10 +6,16 @@ import destinationData from './test-data/destination-test-data';
 describe("Destination", () => {
   let destination;
   beforeEach(() => {
-    destination = new Destination();
+    destination = new Destination(destinationData[0]);
   });
 
   it('Should be an instance of a destination', () => {
     expect(destination).to.be.an.instanceof(Destination);
   });
+
+  describe("Destination Properties", () => {
+    it('Should have an id', () => {
+      expect(destination.id).to.eq(1);
+    });
+  })
 })
