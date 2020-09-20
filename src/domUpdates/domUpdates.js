cd0e2e82e0b8be8dbd29544f81c4a4884db850a2
Nathan Darrington <npdarrington@icloud.com>
2020-09-19 23:29:19 -0600

    Created displayTravelerPastTrips method and buildHTMLForTrips helper method

--- src/domUpdates/domUpdates.js
+++ src/domUpdates/domUpdates.js
@@ -13,11 +13,42 @@ const domUpdates = {
 
   updatePageOnLogin() {
     this.displayCurrentTraveler();
+    this.displayTravelerPastTrips();
   },
 
   displayCurrentTraveler() {
     document.querySelector('.travel-background-cta').innerText = `Let's book your next getaway ${this.currentTraveler.getFirstName()}!`;
   },
+
+  displayTravelerPastTrips() {
+    let tripPreviousDOM = document.querySelector('.trip-previous');
+    let tripPreviousDOMTitle = document.querySelector('.trip-previous > .trip-card-title');
+    this.currentTraveler.sortTripsByStatus(this.todaysDate, this.allTripsData);
+    if (!this.currentTraveler.pastTrips.length) {
+      tripPreviousDOMTitle.innerText = `You have no past trips`;
+    } else {
+      this.currentTraveler.pastTrips.forEach(trip => {
+        let displayTrip = this.buildHTMLForTrips(trip);
+        tripPreviousDOM.insertAdjacentHTML('beforeend', displayTrip);
+      });
+    }
+  },
+
+  buildHTMLForTrips(trip) {
+    let destinationData = this.allDestinationData.find(destination => destination.id === trip.destinationID);
+    return `
+      <section class="trip-information">
+        <article class="trip-picture">
+          <img src="${destinationData.image}" alt="${destinationData.alt}">
+        </article>
+        <article class="trip-details">
+          <h4>${destinationData.destination}</h4>
+          <h4>${trip.date} for ${trip.duration} days</h4>
+          <h4>Brought ${trip.travelers} travelers</h4>
+        </article>
+      </section>
+    `;
+  }
 }
 
 export default domUpdates;
