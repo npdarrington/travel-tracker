4cc3589a873fa4f5c560c3d51a8a56bade297905
Nathan Darrington <npdarrington@icloud.com>
2020-09-19 22:14:44 -0600

    Create getAllTripsData function and added catch error statements to promises

--- src/index.js
+++ src/index.js
@@ -5,9 +5,25 @@ import './images/turing-logo.png'
 import fetches from './fetch';
 import domUpdates from './domUpdates/domUpdates';
 import Traveler from './Traveler';
+import Trip from './Trip';
 import moment from 'moment';
 
 let currentTraveler, todaysDate;
+let allTripsData = [];
+
+const getAllTripsData = () => {
+  return fetches.getAllTrips()
+    .then(data => {
+      data.forEach(trip => {
+        let newTrip = new Trip(trip);
+        allTripsData.push(newTrip);
+      });
+    })
+    .catch(err => {
+      console.log(err.message);
+      return `Something went wrong and we were not able to get your trip data. Please refesh and try again!`;
+    });
+}
 
 const getTravelerData = () => {
   return fetches.getAllTravelers()
@@ -16,7 +32,11 @@ const getTravelerData = () => {
       todaysDate = moment().format('YYYY/MM/DD');
       domUpdates.setGlobalData(currentTraveler, todaysDate);
       domUpdates.updatePageOnLogin();
-    });
+    })
+    .catch(err => {
+      console.log(err.message);
+      return `Something went wrong and we were not able to get your data. Please refesh and try again!`;
+    })
 }
 
-window.addEventListener('load', getTravelerData);
+window.addEventListener('load', getAllTripsData);
