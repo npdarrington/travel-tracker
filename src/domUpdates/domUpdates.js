0211304dd2d282cc2357495ba2990160417e0b77
Nathan Darrington <npdarrington@icloud.com>
2020-09-19 19:56:03 -0600

    Created setGlobalData for domUpdates object

--- src/domUpdates/domUpdates.js
+++ src/domUpdates/domUpdates.js
@@ -1,6 +1,11 @@
 const domUpdates = {
   currentUser: null,
   todaysDate: null,
+
+  setGlobalData(user, todaysDate) {
+    this.currentUser = user;
+    this.todaysDate = todaysDate;
+  }
 }
 
 export default domUpdates;
