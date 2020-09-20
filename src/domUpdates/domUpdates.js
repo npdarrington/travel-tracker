26b11d7f55f9392ab738efcb45b113dc86d09cc5
Nathan Darrington <npdarrington@icloud.com>
2020-09-19 22:33:11 -0600

    Added allTripsData and allDestinationsData global vars and updated setGlobalData to set accordingly

--- src/domUpdates/domUpdates.js
+++ src/domUpdates/domUpdates.js
@@ -1,10 +1,14 @@
 const domUpdates = {
   currentTraveler: null,
   todaysDate: null,
+  allTripsData: null,
+  allDestinationData: null,
 
-  setGlobalData(user, todaysDate) {
+  setGlobalData(user, todaysDate, allTripsData, allDestinationData) {
     this.currentTraveler = user;
     this.todaysDate = todaysDate;
+    this.allTripsData = allTripsData;
+    this.allDestinationData = allDestinationData;
   },
 
   updatePageOnLogin() {
@@ -13,7 +17,7 @@ const domUpdates = {
 
   displayCurrentTraveler() {
     document.querySelector('.travel-background-cta').innerText = `Let's book your next getaway ${this.currentTraveler.getFirstName()}!`;
-  }
+  },
 }
 
 export default domUpdates;
