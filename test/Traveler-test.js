a839bdc5a8064ed85d567546e6bd6b0400dac140
Nathan Darrington <npdarrington@icloud.com>
2020-09-21 17:37:28 -0600

    Added a test for getYTDTotal method

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,12 +1,14 @@
 const { expect } = require("chai");
 
+import destinationData from './test-data/destination-test-data';
 import travelerData from './test-data/traveler-test-data';
 import tripsData from './test-data/trips-test-data';
+import Destination from '../src/Destination';
 import Traveler from '../src/Traveler';
 import Trip from '../src/Trip';
 
 describe('Traveler', () => {
-  let traveler, trip, trip1, trip2, trip3, trip4, allTrips;
+  let traveler, trip, trip1, trip2, trip3, trip4, trip5, allTrips, allDestinations;
 
   beforeEach(() => {
     traveler = new Traveler(travelerData[0]);
@@ -15,7 +17,13 @@ describe('Traveler', () => {
     trip2 = new Trip(tripsData[2]);
     trip3 = new Trip(tripsData[3]);
     trip4 = new Trip(tripsData[4]);
-    allTrips = [trip, trip1, trip2, trip3, trip4];
+    trip5 = new Trip(tripsData[5]);
+    allTrips = [trip, trip1, trip2, trip3, trip4, trip5];
+    allDestinations = [];
+    destinationData.forEach(destination => {
+      let newDestination = new Destination(destination);
+      allDestinations.push(newDestination);
+    })
   });
 
   describe('Traveler Properties', () => {
@@ -65,12 +73,18 @@ describe('Traveler', () => {
   
     it('Sort a travelers destination trip data into upcoming trips for current trips that have been approved', () => {
       traveler.sortTripsByStatus("2019/09/18", allTrips);
-      expect(traveler.upcomingTrips[0]).to.deep.eq(allTrips[0]);
+      expect(traveler.upcomingTrips[0]).to.deep.eq(allTrips[0, 5]);
     });
   
     it('Sort a travelers destination trip data into pending trips for current trips that have not been approved', () => {
       traveler.sortTripsByStatus("2019/09/18", allTrips);
       expect(traveler.pendingTrips[0]).to.deep.eq(allTrips[1]);
+    });
+  });
+
+  describe('Traveler getYTDTotal', () => {
+    it('Should show how much a user has spent for the entire year, not including pending trips', () => {
+      expect(traveler.getYTDTotal('2020', allTrips, allDestinations)).to.eq('12595.00');
     });
   });
 });
